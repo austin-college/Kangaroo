41394b0f082783de8fb1238bfe8925936981824c
Phillip Cohen <pc@phillipcohen.net>
2011-09-25 13:00:50 -0500

    Search is now focused by default.

--- web-app/js/search.js
+++ web-app/js/search.js
@@ -1,15 +1,13 @@
-var table;
-
 $.fn.dataTableExt.oStdClasses.sSortAsc = "headerSortDown";
 $.fn.dataTableExt.oStdClasses.sSortDesc = "headerSortUp";
 
 $(document).ready(function() {
 
 
-    table = $('#classTable').dataTable({
+    $('#classTable').dataTable({
         "bProcessing": true,
         "aaSorting": [
-            [0,'asc'] // Sort by YTM.
+            [1,'asc'] // Sort by YTM.
         ],
         "sAjaxSource": contextPath + "/search/getClassesForTable",
         "fnServerData": function (sSource, aoData, fnCallback) {
@@ -27,23 +25,9 @@ $(document).ready(function() {
             "sInfoEmpty": "Showing no classes",
             "sInfoFiltered": "(filtered from _MAX_ total classes)",
             "sSearch":"Search for anything:"
-        }
+        },
+        "iDisplayLength": 15
     });
-});
 
-/*
- * Runs an AJAX query on the server, then populates the table with the results.
- */
-function runSearch() {
-    $.ajax({
-        url: contextPath + "/search/getClassesForTable",
-        dataType: 'json',
-        data: { s: $("#courseSearch").val() },
-        success: function(response) {
-            $('#resultsArea').html("");
-            $.each(response, function(index, value) {
-                $("#resultsArea").append("<h2>" + value.name + "</h2>");
-            });
-        }
-    });
-}
+    $("#classTable_filter input").focus();
+});
