61d1a4206ca444601304aa2914a458bd361185a6
Phillip Cohen <pc@phillipcohen.net>
2011-09-25 15:37:17 -0500

    The redis cache is now invalidated on startup. If the course list is empty, it's loaded from file.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,13 +1,21 @@
-import grails.converters.JSON
 import coursesearch.Course
+import grails.converters.JSON
+import redis.clients.jedis.Jedis
 
 class BootStrap {
 
+    def courseListParseService
+    def redisService
+
     def init = { servletContext ->
-         // Customize how objects are formatted to JSON.
+        // Customize how objects are formatted to JSON.
         JSON.registerObjectMarshaller(Course) {
             return [id: it.id, name: it.name, items: it.items];
         }
+
+        if (Course.count() == 0)
+            courseListParseService.parseScrape();
+        redisService.withRedis { Jedis redis -> redis.del("courses")}
     }
 
     def destroy = {
