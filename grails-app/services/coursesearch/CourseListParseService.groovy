ce14bbe70a962b9b65f07ff138e32d4e10d1851c
Phillip Cohen <pc@phillipcohen.net>
2011-09-30 11:08:59 -0500

    The list of courses is now downloaded from a server and does not need to be in the working directory.

--- grails-app/services/coursesearch/CourseListParseService.groovy
+++ grails-app/services/coursesearch/CourseListParseService.groovy
@@ -16,92 +16,103 @@ class CourseListParseService {
 
         int totalSaved = 0;
         int totalErrors = 0;
-        for (int i = 0; i <= 20; i++) {
 
-            def file = readAndConvertToXml("scrapes/${i}.html");
 
-            def table = getSectionsTable(file);
+        def tfile = new File('scrapes.zip').newOutputStream()
+        tfile << new URL('http://phillipcohen.net/scrapes.zip').openStream()
+        tfile.close()
 
-            int rowNum = 0;
-            int numSaved = 0;
-            println "Parsing file ${i}.html..."
-            table.tbody.tr.each { row ->
+        def zipFile = new java.util.zip.ZipFile('scrapes.zip')
 
-                if (rowNum++ > 2) {
+        zipFile.entries().each { zip ->
 
-                    def course = new Course(name: row.td[8].text());
-                    try {
-                        course.open = (row.td[2].text() == 'Open');
+            def text = zipFile.getInputStream(zip).text;
+            if (text.length() > 0) {
+                def file = readAndConvertToXml(text);
 
-                        def seatsSplit = row.td[3].toString().split('/');
-                        if (seatsSplit.size() == 2) {
-                            course.capacity = (seatsSplit[1] as Integer);
-                            course.seatsUsed = course.capacity - (seatsSplit[0] as Integer);
-                        }
+                def table = getSectionsTable(file);
 
-                        course.instructorConsentRequired = (row.td[4] == 'Y');
-                        course.reqCode = (row.td[5])
-                        course.zap = row.td[6].toString().length() > 0 ? Integer.parseInt(row.td[6].toString().trim()) : 0;
+                int rowNum = 0;
+                int numSaved = 0;
+                println "Parsing file ${zip.name}..."
+                table.tbody.tr.each { row ->
 
-                        def departmentString = row.td[7].toString().split('\\*')[0];
-                        course.department = Department.findByCode(departmentString) ?: new Department(code: departmentString, name: departmentString).save();
-                        def courseNumber = row.td[7].toString().split('\\*')[1].toString();
+                    if (rowNum++ > 2) {
 
-                        // Some courses are labs
-                        if (courseNumber.endsWith("L")) {
-                            course.isLab = true;
-                            course.courseNumber = (courseNumber[0..-2] as Integer);
-                        }
-                        else
-                            course.courseNumber = (courseNumber as Integer);
+                        def course = new Course(name: row.td[8].text());
+                        try {
+                            course.open = (row.td[2].text() == 'Open');
 
-                        course.section = (row.td[7].toString().split('\\*')[2] as Character);
-                        def instructors = row.td[9].div.input.@value.toString()
-
-                        def enrollments = [];
-                        instructors.split('<BR>').each { name ->
-                            def professor = Professor.findByName(name.trim());
-
-                            if (!professor) {
-                                professor = new Professor(name: name.trim());
-                                professor.save()
-                                println professor.errors
+                            def seatsSplit = row.td[3].toString().split('/');
+                            if (seatsSplit.size() == 2) {
+                                course.capacity = (seatsSplit[1] as Integer);
+                                course.seatsUsed = course.capacity - (seatsSplit[0] as Integer);
                             }
-                            enrollments << new Teaching(course: course, professor: professor);
-                        }
+
+                            course.instructorConsentRequired = (row.td[4] == 'Y');
+                            course.reqCode = (row.td[5])
+                            course.zap = row.td[6].toString().length() > 0 ? Integer.parseInt(row.td[6].toString().trim()) : 0;
+
+                            def departmentString = row.td[7].toString().split('\\*')[0];
+                            course.department = Department.findByCode(departmentString) ?: new Department(code: departmentString, name: departmentString).save();
+                            def courseNumber = row.td[7].toString().split('\\*')[1].toString();
+
+                            // Some courses are labs
+                            if (courseNumber.endsWith("L")) {
+                                course.isLab = true;
+                                course.courseNumber = (courseNumber[0..-2] as Integer);
+                            }
+                            else
+                                course.courseNumber = (courseNumber as Integer);
+
+                            course.section = (row.td[7].toString().split('\\*')[2] as Character);
+                            def instructors = row.td[9].div.input.@value.toString()
+
+                            def enrollments = [];
+                            instructors.split('<BR>').each { name ->
+                                def professor = Professor.findByName(name.trim());
+
+                                if (!professor) {
+                                    professor = new Professor(name: name.trim());
+                                    professor.save()
+                                }
+                                enrollments << new Teaching(course: course, professor: professor);
+                            }
 
 
-                        course.room = row.td[10].toString();
-                        course.schedule = row.td[11].toString();
-                        course.comments = row.td[12].toString();
-                        if (!course.save()) {
-                            println "ERROR in saving ${course}..."
-                            course.errors.each { error -> println "\t${error}"}
+                            course.room = row.td[10].toString();
+                            course.schedule = row.td[11].toString();
+                            course.comments = row.td[12].toString();
+                            if (!course.save()) {
+                                println "ERROR in saving ${course}..."
+                                course.errors.each { error -> println "\t${error}"}
+                            }
+                            enrollments.each { it.save(); }
+                            numSaved++;
+                            totalSaved++;
                         }
-                        enrollments.each { it.save(); }
-                        numSaved++;
-                        totalSaved++;
-                    }
-                    catch (Exception e) {
-                        println "Error during course conversion of '${course}' (table row #${rowNum - 1})...";
-                        println e;
-                        e.printStackTrace()
-                        throw e;
-                        totalErrors++;
-                        return "ERROR"
+                        catch (Exception e) {
+                            println "Error during course conversion of '${course}' (table row #${rowNum - 1})...";
+                            println e;
+                            e.printStackTrace()
+                            throw e;
+                            totalErrors++;
+                            return "ERROR"
+                        }
                     }
                 }
+                println "${numSaved} saved"
             }
-            println "${numSaved} saved"
         }
 
+        new File('scrapes.zip').delete()
         "\nDONE!\n${totalSaved} saved (${Teaching.count()} enrollments); ${totalErrors} errors."
     }
 
-    GPathResult readAndConvertToXml(String filename) {
+    GPathResult readAndConvertToXml(String text) {
         // Clean any messy HTML
         def cleaner = new HtmlCleaner()
-        def node = cleaner.clean(new File(filename).text)
+        def node = cleaner.clean(text)
 
         // Convert from HTML to XML
         def props = cleaner.getProperties()
