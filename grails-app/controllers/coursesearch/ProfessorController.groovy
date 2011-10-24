60155503df21a1eeae8e2b4aa6a9e40586bca666
Phillip Cohen <pc@phillipcohen.net>
2011-10-24 12:58:37 -0500

    Started to add professor schedule parsing. Currently it just acquire a list of a professor's meetingTimes.

--- grails-app/controllers/coursesearch/ProfessorController.groovy
+++ grails-app/controllers/coursesearch/ProfessorController.groovy
@@ -8,8 +8,19 @@ class ProfessorController {
 
     def show = {
         def professor = Professor.get(params.id)
+        getSchedule(professor)
         if (professor) {
             [professor: professor]
         }
+    }
+
+    def getSchedule(Professor professor) {
+
+        def days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        def schedule = ["Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": []];
+
+        // Get all of this professor's meeting times.
+        List<MeetingTime> meetingTimes = []
+        professor.coursesTeaching.each { course -> meetingTimes.addAll(course.meetingTimes) }
     }
 }
