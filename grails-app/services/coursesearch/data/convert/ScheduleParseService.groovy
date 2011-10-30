335706f9325de18abdad8a604bceeb42ec6c94aa
Phillip Cohen <pc@phillipcohen.net>
2011-10-30 13:36:53 -0500

    Cleaned up schedule parsing a bit.

--- grails-app/services/coursesearch/data/convert/ScheduleParseService.groovy
+++ grails-app/services/coursesearch/data/convert/ScheduleParseService.groovy
@@ -19,39 +19,43 @@ class ScheduleParseService {
             if (composite.contains("TBA"))
                 return null;
 
-            // Make sure that if all the day codes are on, there's a space after them. (WebHopper won't do so)
+            // Make sure that there's a space after the day codes. (WebHopper won't have one if they are all on)
             if (composite.startsWith("MTWTHF"))
                 composite = composite.replaceAll("MTWTHF", "MTWTHF ")
 
             // Split the string into three parts ("MWF", "09:00AM", "10:00AM") without padding.
             def parts = (List<String>) composite.split(" ").findAll { it.length() > 0 }
 
-            def meetingTime = new MeetingTime(startTime: parts[1], endTime: parts[2])
-
-            // Convert the "MTWTHF" string into meets*** booleans.
-            meetingTime.with {
-
-                def days = parts[0];
-
-                // Check "TH" first and remove it so "T" does not match.
-                if (days.contains("TH"))
-                    meetsThursday = true;
-                days = days.replaceAll("TH", "")
-
-                // Check for the rest
-                if (days.contains('M'))
-                    meetsMonday = true;
-                if (days.contains('T'))
-                    meetsTuesday = true;
-                if (days.contains('W'))
-                    meetsWednesday = true;
-                if (days.contains('F'))
-                    meetsFriday = true;
-            }
-            return meetingTime;
+            // Set the meeting time's properties and return.
+            return setDayCodes(new MeetingTime(startTime: parts[1], endTime: parts[2]), parts[0]);
         }
         catch (Exception e) {
-            println "Error parsing schedule $composite..."
+            println "Error parsing schedule $composite ($e)..."
         }
+    }
+
+    /**
+     * Sets the meeting time's "meetsXDay" properties to match the "MWF" codes in the string.
+     */
+    static MeetingTime setDayCodes(MeetingTime meetingTime, String days) {
+        meetingTime.with {
+
+            // Check "TH" first and remove it so "T" does not match.
+            if (days.contains("TH"))
+                meetsThursday = true;
+            days = days.replaceAll("TH", "")
+
+            // Check for the rest
+            if (days.contains('M'))
+                meetsMonday = true;
+            if (days.contains('T'))
+                meetsTuesday = true;
+            if (days.contains('W'))
+                meetsWednesday = true;
+            if (days.contains('F'))
+                meetsFriday = true;
+        }
+
+        return meetingTime
     }
 }
