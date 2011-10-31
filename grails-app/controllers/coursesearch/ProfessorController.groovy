c3ecc407e663003598c91f2ebbd434a8cf08ed0b
Phillip Cohen <pc@phillipcohen.net>
2011-10-31 12:23:16 -0500

    Devised a way for the ProfessorController's getSchedule() and getOfficeHours() to share code, using the new function projectToWeek().

--- grails-app/controllers/coursesearch/ProfessorController.groovy
+++ grails-app/controllers/coursesearch/ProfessorController.groovy
@@ -18,33 +18,30 @@ class ProfessorController {
         }
     }
 
+    /**
+     * Returns a JSON list of this professor's class schedule for the week.
+     */
     def getSchedule = {
 
         def professor = Professor.get(params.id as Long);
 
         if (professor) {
 
-            def events = [];
+            def events = []
 
             professor.coursesTeaching.each { course ->
-                course.meetingTimes.each { meetingTime ->
-                    meetingTime.daysAsCodes.each { day ->
-
-                        def date = getUpcomingWeekday(dayToOffset(day));
-
-                        def startDate = setTime(date, 'hh:mma', meetingTime.startTime);
-                        def endDate = setTime(date, 'hh:mma', meetingTime.endTime);
-
-                        events << [title: course.name, allDay: false, start: startDate, end: endDate,
-                                url: g.createLink(controller: "course", action: "show", id: course.id)];
-                    }
+                projectToWeek(course.meetingTimes).each { time ->
+                    events << [title: course.name, allDay: false, start: time.startDate, end: time.endDate,
+                            url: g.createLink(controller: "course", action: "show", id: course.id)]
                 }
             }
-
             render(events as JSON);
         }
     }
 
+    /**
+     * Returns a JSON list of this professor's office hours for the week.
+     */
     def getOfficeHours = {
         def professor = Professor.get(params.id as Long);
 
@@ -52,23 +49,36 @@ class ProfessorController {
 
             def events = [];
 
-            professor.officeHours.each { meetingTime ->
-                meetingTime.daysAsCodes.each { day ->
-
-                    def date = getUpcomingWeekday(dayToOffset(day));
-
-                    def startDate = setTime(date, 'hh:mma', meetingTime.startTime);
-                    def endDate = setTime(date, 'hh:mma', meetingTime.endTime);
-
-                    events << [title: "Office Hours", allDay: false, start: startDate, end: endDate,
-                            url: g.createLink(controller: "course", action: "show")];
-                }
+            projectToWeek(professor.officeHours).each { time ->
+                events << [title: "Office Hours", allDay: false, start: time.startDate, end: time.endDate]
             }
 
             render(events as JSON);
         }
     }
 
+    /**
+     * Given a list of metingTimes, returns a list of startTimes and endTimes for this week.
+     */
+    def projectToWeek(meetingTimes) {
+        def events = []
+        meetingTimes.each { meetingTime ->
+            meetingTime.daysAsCodes.each { day ->
+
+                // Project the date forward.
+                def date = getUpcomingWeekday(dayToOffset(day));
+                def startDate = setTime(date, 'hh:mma', meetingTime.startTime);
+                def endDate = setTime(date, 'hh:mma', meetingTime.endTime);
+
+                events << [startDate: startDate, endDate: endDate];
+            }
+        }
+        return events;
+    }
+
+    /**
+     * Sets the given date's time to the given format string.
+     */
     Date setTime(Date date, String format, String time) {
 
         def newDate = new Date().parse(format, time);
@@ -86,6 +96,7 @@ class ProfessorController {
             date -= (date[Calendar.DAY_OF_WEEK] - Calendar.MONDAY)
 
         date += dayOfWeek;
+        date;
     }
 
     int dayToOffset(code) {
