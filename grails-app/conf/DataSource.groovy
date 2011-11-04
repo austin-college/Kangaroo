cd568928cb933cef8c9301c6058c593a66ad5160
Phillip Cohen <pc@phillipcohen.net>
2011-11-03 21:13:48 -0500

    Production db is now create-drop.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -33,7 +33,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+            dbCreate = "update" // one of 'create', 'create-drop','update'
             url = "jdbc:mysql://localhost:3306/course_search?useUnicode=true&amp;characterEncoding=utf8"
         }
     }
