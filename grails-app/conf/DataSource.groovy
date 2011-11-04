3d306a543a446bcc6e70ed095a9b9bd42277dca1
Phillip Cohen <pc@phillipcohen.net>
2011-11-03 20:35:33 -0500

    Development database is now create-drop.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -15,8 +15,8 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:kangarooTestDb"
+            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+            url = "jdbc:hsqldb:mem:kangarooDevDb"
             driverClassName = "org.hsqldb.jdbcDriver"
             username = "sa"
             password = ""
