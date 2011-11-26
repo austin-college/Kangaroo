5594fdfd36b4c445c33a39385e2ed94ad5d36411
Phillip Cohen <pc@phillipcohen.net>
2011-11-26 11:27:59 -0600

    Some cleanup to the MajorDataService.

--- grails-app/services/coursesearch/data/prefill/MajorDataService.groovy
+++ grails-app/services/coursesearch/data/prefill/MajorDataService.groovy
@@ -3,38 +3,26 @@ package coursesearch.data.prefill
 import coursesearch.Department
 import coursesearch.Major
 import grails.converters.JSON
+import org.springframework.transaction.annotation.Transactional
 
+/**
+ * Fills in data about Austin College's majors and minors.
+ */
 class MajorDataService {
 
-    static transactional = true
-
+    @Transactional
     def setUpMajors() {
 
         // Remove the existing majors.
         Major.list().each { it.delete(flush: true) }
 
-        listFromServer.each { data ->
+        majorsFromServer.each { data ->
 
             def department = Department.findByName(data.department);
 
             if (department) {
 
-                String description = data.description;
-
-                // Wrap the description in <p> tags if it isn't already.
-                if (!description.startsWith("<p>") && !description.endsWith("</p>"))
-                    description = "<p>$description</p>";
-
-                // Bold the "a major in xxx" part automatically.
-                int startPosition = description.toLowerCase().indexOf("a ");
-                int endPosition = description.toLowerCase().indexOf(data.name.toString().toLowerCase());
-                if (startPosition > -1 && endPosition > startPosition)
-                    description = description.substring(0, startPosition) + "<strong>" + description.substring(startPosition, endPosition + data.name.toString().length()) + "</strong>" + description.substring(endPosition + data.name.toString().length());
-                else
-                    println "'a' not found in $description"
-
-                def major = new Major(name: data.name, description: description,
-                        isMajor: data.isMajor, department: department);
+                def major = new Major(name: data.name, description: improveDescription(data.description), isMajor: data.isMajor, department: department);
 
                 if (!major.save())
                     println major.errors.toString()
@@ -46,5 +34,25 @@ class MajorDataService {
         println "${Major.count()} majors saved successfully"
     }
     
-    List getListFromServer() { (List) JSON.parse(new URL("https://raw.github.com/austin-college/data/master/majors.json").text); }
+    List getMajorsFromServer() { (List) JSON.parse(new URL("https://raw.github.com/austin-college/data/master/majors.json").text); }
+
+    /**
+     * Makes improvements to the HTML description of a major.
+     */
+    String improveDescription(String description, String majorName) {
+
+        // Wrap the description in <p> tags if it isn't already.
+        if (!description.startsWith("<p>") && !description.endsWith("</p>"))
+            description = "<p>$description</p>";
+
+        // Bold the "a major in xxx" part automatically.
+        int startPosition = description.toLowerCase().indexOf("a ");
+        int endPosition = description.toLowerCase().indexOf(majorName.toLowerCase()) + majorName.length();
+        if (startPosition > -1 && endPosition > startPosition)
+            description = description.substring(0, startPosition) + "<strong>" + description.substring(startPosition, endPosition) + "</strong>" + description.substring(endPosition);
+        else
+            println "'a' not found in $description"
+
+        description;
+    }
 }
