8ad684fabb14ace1db9406ad450d6d3cd005b9cb
Phillip Cohen <pc@phillipcohen.net>
2012-01-30 11:24:54 -0600

    Switched professor profile view over to 12SP (todo: centralize/automate this).

--- grails-app/controllers/coursesearch/ProfessorController.groovy
+++ grails-app/controllers/coursesearch/ProfessorController.groovy
@@ -37,7 +37,7 @@ class ProfessorController {
         }
 
         // Next check the dates for each of the professor's courses, to see if the professor is in class.
-        for (def course: professor.coursesTeaching.findAll { it.term == Term.findOrCreate("11FA")}) {
+        for (def course: professor.coursesTeaching.findAll { it.term == Term.findOrCreate("12SP")}) {
             for (def time: ScheduleProjectService.projectToWeek(course.meetingTimes)) {
 
                 if (isDateBetween(new Date(), time.startDate, time.endDate))
@@ -62,7 +62,7 @@ class ProfessorController {
             def events = []
 
             // For every course, convert its meeting times into real dates...
-            professor.coursesTeaching.findAll { it.term == Term.findByShortCode("11FA") }.each { course ->
+            professor.coursesTeaching.findAll { it.term == Term.findByShortCode("12SP") }.each { course ->
                 ScheduleProjectService.projectToWeek(course.meetingTimes).each { time ->
 
                     // ...then add them to the calendar.
