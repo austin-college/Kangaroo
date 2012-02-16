57ba9dd82c6cc33fea0b4f72efa04e058bdfb839
Phillip Cohen <pc@phillipcohen.net>
2012-02-15 18:26:09 -0600

    Made DataFetcherJob run every 15 minutes.

--- grails-app/jobs/coursesearch/DataFetcherJob.groovy
+++ grails-app/jobs/coursesearch/DataFetcherJob.groovy
@@ -5,7 +5,7 @@ class DataFetcherJob {
     def backendDataService
 
     static triggers = {
-        simple name: 'mySimpleTrigger', startDelay: 2 * 60 * 1000, repeatInterval: 60 * 1000
+        simple name: 'every15Minutes', startDelay: 2 * 60 * 1000, repeatInterval: 60 * 1000 * 15
     }
 
     def execute() { backendDataService.upgradeAllIfNeeded() }
