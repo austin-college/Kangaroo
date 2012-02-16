ce4ac705d202300d56f8e72fcb349f64e0d45e16
Phillip Cohen <pc@phillipcohen.net>
2012-02-16 00:58:37 -0600

    Renamed the WAR file to kangaroo.war again.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.war.file = "target/ROOT.war"
+grails.project.war.file = "target/kangaroo.war"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
