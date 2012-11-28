a91c4aa474f809e5bad5f0438ce0388306661f34
Phillip Cohen <pc@phillipcohen.net>
2012-11-27 16:28:14 -0800

    Changed Requirement's isDisciplinaryMajor JSON boolean to a "type" string.

--- grails-app/domain/kangaroo/Requirement.groovy
+++ grails-app/domain/kangaroo/Requirement.groovy
@@ -12,6 +12,10 @@ class Requirement {
 
     String id // The code ("HU", "SS")
     String name
+
+    // True if this isn't a "requirement" per se, but a marker for courses that satisfy inter-disciplinary majors (Environmental Studies, Asian Studies, Film Studies, etc.)
+    // These majors get requirement codes because their courses appear across many departments, and otherwise it's hard to search for them.
+    // @todo refactor to "type" enum
     boolean isInterdisciplinaryMajor
 
     static constraints = {
@@ -31,10 +35,11 @@ class Requirement {
         if (Requirement.get(object.id))
             return Requirement.get(object.id);
 
-        def requirement = new Requirement(name: object.name, isInterdisciplinaryMajor: object.isInterdisciplinaryMajor)
+        def requirement = new Requirement(name: object.name, isInterdisciplinaryMajor: (object.type == "interdisciplinaryMajor"))
         requirement.id = object.id;
         return (Requirement) AppUtils.saveSafely(requirement);
     }
 
-    def toJsonObject() { [id: id, name: name, isInterdisciplinaryMajor: isInterdisciplinaryMajor]; }
+    def toJsonObject() { [id: id, name: name, type: isInterdisciplinaryMajor ? "interdisciplinaryMajor" : "requirement"]; }
+
 }
